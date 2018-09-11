da81eb86085b4e4b12989cf14c0ba30540545f41
William Kulha <kulha.william@gmail.com>
2018-09-11 12:17:33 -0400

    Add functionality so that when a user submits the form, a new contact is added to the well below.

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,36 @@
+//var addNewContact = document.createElement('div');
+//  addNewContact.innerHTML = '<div class="panel panel-default">' +
+//  '<div class="panel-heading">' +
+//  '<div class="panel-title">' +
+//  '<span class="contactName"></span>' +
+//  '</div></div>' +
+//  '<div class="panel-body">' +
+//  '<p><span class="contactNumber"></span></p>' +
+//  '<p><span class="contactEmail"></span></p>' +
+//  '<p><span class="contactStreetAddress"></span></p>' +
+//  '</div></div>';
+//  $(addNewContact).appendto('contacts');
+
+function combineInfo(name, number, email, address) {
+  return ("<p class=\"clickable\">" + name + "<br>" + number + "<br>" + email + "<br>" + address + "</p>");
+};
+
+
+
+
 
 $(document).ready(function(){
-  $("form#newContact").submit(function(event){
+  $("form#newContact").submit(function(event) {
     event.preventDefault();
     var contactName = $("input#contactName").val();
     var contactNumber = $("input#contactNumber").val();
     var contactEmail = $("input#contactEmail").val();
     var contactStreetAddress = $("input#contactStreetAddress").val();
-    debugger;
+    var contact = combineInfo(contactName, contactNumber, contactEmail, contactStreetAddress);
+    $('#contactList').append(contact);
+    $("div#contactList").children("p.clickable").first().click(function(){
+      this.remove();
+    });
   });
+
 });
